0986cb2413cf6efccc8cacdfa63e8326aceb96a3
AirenMarie <astringer92@gmail.com>
2024-08-05 17:27:49 -0400

    call APIs for animation, update player's position

--- platformer.js
+++ platformer.js
@@ -61,7 +61,11 @@ class Player {
 
 const player = new Player();
 
-const animate = () => {};
+const animate = () => {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+};
 
 const startGame = () => {
   canvas.style.display = "block";
